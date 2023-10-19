3a857cefb6bdb6350339a646f013e19d758be6b4
lyan <lyan.pangestu@formi.id>
2023-10-19 15:35:03 +0700

    fix: add support for async root module

--- src/bugsnag.interfaces.ts
+++ src/bugsnag.interfaces.ts
@@ -1,10 +1,11 @@
 import { Bugsnag } from '@bugsnag/js';
+import {ModuleMetadata} from "@nestjs/common/interfaces";
 
 export interface BugsnagClientInterface extends Bugsnag.Client {}
 
 export type BugsnagModuleOptions = string | Bugsnag.IConfig;
 
-export interface BugsnagModuleAsyncOptions {
+export interface BugsnagModuleAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
   useFactory: (
     ...args: any[]
   ) => Promise<BugsnagModuleOptions> | BugsnagModuleOptions;
